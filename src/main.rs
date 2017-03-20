c5ea82dbc52d906d54aee56fffa9ec641f9ecfce
artem.ivanov <artem.ivanov@dsr-company.com>
2017-03-20 13:57:53 +0300

    Converts cargo test output into a xunit report

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+extern crate sxd_document;
+
+mod element;
+mod parser;
+
+use std::fs::File;
+use std::process::Command;
+use sxd_document::Package;
+use sxd_document::writer::format_document;
+
+
+fn main() {
+
+    println!("Running tests");
+
+    let output = Command::new("cargo")
+        .arg("test")
+        .output()
+        .expect("failed to execute command");
+
+    println!("Running tests -> done");
+
+    println!("Parsing of results");
+
+    let report = parser::parse_test_report(output);
+
+    println!("Parsing of results -> done");
+
+    println!("Building xunit test report");
+
+    let package = Package::new();
+    let document = element::build_xunit_report(&package, report);
+
+    println!("Building xunit test report -> done");
+
+    println!("Writing report");
+
+    let mut f = File::create("test-results.xml").unwrap();
+
+    format_document(&document, &mut f)
+        .ok()
+        .expect("unable to output XML");
+
+    println!("Writing report -> done");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert!(true);
+    }
+
+    #[test]
+    fn it_does_not_works() {
+        assert!(false);
+    }
+}
